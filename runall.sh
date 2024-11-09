5c0be224034a2edc57c4a4f2f7fa85f707b053ac
Max Rydahl Andersen <max@xam.dk>
2024-11-09 11:21:08 +0100

    fix -q

--- runall.sh
+++ runall.sh
@@ -1,3 +1,3 @@
 for model in llama3.1 granite3-dense starcoder2; do
-    jbang -q --fresh -Dquarkus.langchain4j.ollama.chat-model.model-id=$model coi.java
+    jbang --quiet --fresh -Dquarkus.langchain4j.ollama.chat-model.model-id=$model coi.java
 done
